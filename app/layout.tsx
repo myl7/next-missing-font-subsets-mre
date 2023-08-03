f8b395881b67921e6f47e724d1cadf833e39a932
myl7 <myl@myl.moe>
2023-08-03 19:03:39 +0800

    Add fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,16 @@
+import { Noto_Serif_SC } from '@next/font/google'
+const notoSerifSC = Noto_Serif_SC({
+  weight: ['400'],
+  // TODO: Google Fonts does not embed the subsets in the comments of CSS files,
+  // so Next.js needs a new way to find available subsets.
+  // subsets: ['chinese-simplified'],
+  subsets: ['latin'],
+  variable: '--font-noto-serif-sc',
+})
+
 export default function RootLayout({ children }) {
   return (
-    <html>
+    <html className={`${notoSerifSC.variable}`}>
       <head />
       <body>{children}</body>
     </html>
